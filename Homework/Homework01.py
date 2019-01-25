f0715d6e4ed216af2e49d47db7c0875f8f1f9971
Gonzales <gonzalesp3907@pclab.uhcl.edu>
2019-01-24 21:24:21 -0600

    note and homework cleanup.

--- Homework/Homework01.py
+++ Homework/Homework01.py
@@ -4,34 +4,31 @@
 # ALGORITHM: How the program works.  This should be structured using short,
 # descriptive phrases that are indented appropriately.
 
+
+# TODO: LOOK UP SOLUTION per in class discussion; answer is in book
+
 (Financial application: loan amortization schedule)
 The monthly payment for a given loan pays the principal and the interest.
 The monthly interest is computed by multiplying the monthly interest rate and the balance (the remaining principal).
 The principal paid for the month is therefore the monthly payment minus the monthly interest.
-Write a program that lets the user enter the loan amount, number of years, and interest rate, and then displays the amortization schedule for the loan.
 
 Note
-
 The balance after the last payment may not be zero. If so, the last payment
 should be the normal monthly payment plus the final balance.
 
 
+# Start
+balance = input("Enter Loan Ammount: ") # The balance is initially the loan amount.
+numberOfYears = input("Enter Loan Term: ")
+monthlyInterestRate = input("Enter Interest Rate: ")
 
-# Variables
-monthlyPayment
-principal
-interest
-monthlyinterest = monthly interest rate * balance
-balance
-loanammount
 
-Hint
+# TODO: display amortization schedule for the loan
 
-Write a loop to display the table. Since the monthly payment is the same
-for each month, it should be computed before the loop. The balance is
-initially the loan amount. For each iteration in the loop, compute the
-interest and principal and update the balance. The loop may look like this:
+# Since the monthly payment is the same for each month, it should be computed before the loop.
+monthlyPayment = principal + interest
 
+# For each iteration in the loop, compute the interest and principal and update the balance.
 for i in range(1, numberOfYears * 12 + 1):
 
     interest = monthlyInterestRate * balance
