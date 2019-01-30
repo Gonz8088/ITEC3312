b8690a07b8002a68330ed0d466afc6119bccf614
Gonzales <212709006@logon.ds.ge.com>
2019-01-30 14:40:11 -0600

    final code cleanup.

--- Homework/Homework01.py
+++ Homework/Homework01.py
@@ -3,8 +3,8 @@
 # ASSIGNMENT: Homework 01 (Financial application: loan amortization schedule)
 # ALGORITHM: 1) prompt user to input Principal, Rate, Term;
 # 2) convert annual interest into monthly interest, and convert term from years to months;
-# 3) caclulate monthly payment using data provided;
-# 4) loop through each monthly payment cycle calculating the interest portion, and
+# caclulate monthly payment using data provided;
+# 3) loop through each monthly payment cycle calculating the interest portion, and
 # principal portion of the payment, decreasing the remaining balance, and printing
 # to standard out these data.
 
@@ -15,7 +15,7 @@ def calc_month_rate(apr):
 def calc_month_payment(p, j, n):
     return p*(j/(1-(1+j)**(-n)))
 
-# Start
+# 1)
 while True:
     try:
         balance = int(input("Enter Loan Ammount: "))
@@ -30,8 +30,6 @@ while True:
     except ValueError:
         print("Invalid Loan Term...")
 
-numberOfMonths = numberOfYears * 12
-
 while True:
     try:
         rate = int(input("Enter Interest Rate: "))
@@ -39,16 +37,19 @@ while True:
     except ValueError:
         print("Invalid Rate Input...")
 
+# 2)
+numberOfMonths = numberOfYears * 12
 monthlyInterestRate = calc_month_rate(rate)
 monthlyPayment = calc_month_payment(balance, monthlyInterestRate, numberOfMonths)
 
-# For each iteration in the loop, compute the interest and principal and update the balance.
+# 3)
 for i in range(1, numberOfYears * 12 + 1):
 
     interest = monthlyInterestRate * balance
-
     principal = monthlyPayment - interest
-
     balance = balance - principal
 
-    print("Month: ", i, "\t", "Interest: ", round(interest, 2), "\t", "Principal: ", round(principal, 2), "\t", "Remaining Balance: ", round(balance, 2))
+    print("Month: ", i, "\t", \
+    "Interest: ", round(interest, 2), "\t", \
+    "Principal: ", round(principal, 2), "\t", \
+    "Remaining Balance: ", round(balance, 2))
