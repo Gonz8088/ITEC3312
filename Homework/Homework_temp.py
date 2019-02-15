a1bb524a2b1f62412ce3dd69a83fa83a716036a3
Gonzales <212709006@logon.ds.ge.com>
2019-02-15 13:28:26 -0600

    updated home work template.

--- Homework/Homework_temp.py
+++ Homework/Homework_temp.py
@@ -1,4 +1,10 @@
 # PROGRAMMER: Paul Gonzales
-# DATE:   month day, 2019
-# ASSIGNMENT:  Homework #00
+# DATE: month day, 2019
+# ASSIGNMENT: Homework #00
 # ALGORITHM: How the program works.  This should be structured using short, descriptive phrases that are indented appropriately.
+
+def main():
+    return None
+
+if __name__ == "__main__":
+    main()
