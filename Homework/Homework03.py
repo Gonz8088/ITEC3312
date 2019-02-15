7e1b9e2584389b35470d9de84cfdc66b27e8ccbf
Gonzales <212709006@logon.ds.ge.com>
2019-02-15 17:12:26 -0600

    home work 3.

---
+++ Homework/Homework03.py
@@ -0,0 +1,37 @@
+# PROGRAMMER: Paul Gonzales
+# DATE: February 21, 2019
+# ASSIGNMENT: Homework 03
+# ALGORITHM: How the program works.  This should be structured using short, descriptive phrases that are indented appropriately.
+
+def getNumber(uppercaseLetter):
+    keypad = {
+    2: 'ABCabc',
+    3: 'DEFdef',
+    4: 'GHIghi',
+    5: 'JKLjkl',
+    6: 'MNOmno',
+    7: 'PQRSpqrs',
+    8: 'TUVtuv',
+    9: 'WXYZwxyz'
+    }
+
+    for kv_pair in iter(keypad.items()):
+        if uppercaseLetter in kv_pair[1]:
+            return kv_pair[0]
+        else:
+            pass
+
+def main():
+    phone_number = input("Enter a string:").replace('-', '')
+    if phone_number.isnumeric():
+        print(phone_number)    # needs to be in xxx-xxx-xxxx format
+    else:
+        for char in phone_number:
+            if char.isalpha():
+                phone_number = phone_number.replace(char, str(getNumber(char)))
+        print(phone_number)    # needs to be in xxx-xxx-xxxx format
+
+    return None
+
+if __name__ == "__main__":
+    main()
