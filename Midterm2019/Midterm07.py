8aaca0fc0768f267f0edde36cafa56e909d20ec6
Gonzales <212709006@logon.ds.ge.com>
2019-03-08 16:22:17 -0600

    added midterm 07.

---
+++ Midterm2019/Midterm07.py
@@ -0,0 +1,12 @@
+# PROGRAMMER: Paul Gonzales
+# DATE: month day, 2019
+# ASSIGNMENT: Midterm #00
+# ALGORITHM: How the program works.  This should be structured using short, descriptive phrases that are indented appropriately.
+
+def count(s, ch):
+    
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
