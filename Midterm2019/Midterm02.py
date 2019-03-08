4bec9901d2b36fe293de77d767fd5fb34b46f678
Gonzales <212709006@logon.ds.ge.com>
2019-03-08 13:08:31 -0600

    finished midterm 02.

--- Midterm2019/Midterm02.py
+++ Midterm2019/Midterm02.py
@@ -3,7 +3,24 @@
 # ASSIGNMENT: Midterm #00
 # ALGORITHM: How the program works.  This should be structured using short, descriptive phrases that are indented appropriately.
 
+from math import pi
+
+def areaCircle(radius: int) -> float:
+    return pi * pow(radius, 2)
+
 def main():
+    medium = 12
+    large = 16
+
+    two_medium = 2 * areaCircle(medium)
+    one_large = areaCircle(large)
+    difference = two_medium - one_large
+
+    print("The area of two medium pizzas is: " + str(round(two_medium)) + " sq.in.")
+    print("The area of one large pizza is: " + str(round(one_large)) + " sq.in.")
+    print("The difference in size is: " + str(round(difference)) + " sq.in.")
+    print("The difference in price is: $3")
+
     return
 
 if __name__ == "__main__":
