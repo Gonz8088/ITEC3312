e13c1ec7828883c57ad8b6738ad40e227cab1ad7
Gonzales <212709006@logon.ds.ge.com>
2019-03-08 15:25:59 -0600

    finished midterm 05.

---
+++ Midterm2019/Midterm05.py
@@ -0,0 +1,31 @@
+# PROGRAMMER: Paul Gonzales
+# DATE: month day, 2019
+# ASSIGNMENT: Midterm #00
+# ALGORITHM: How the program works.  This should be structured using short, descriptive phrases that are indented appropriately.
+
+def oldMacDonald(pet: str) -> str:
+    if pet == "cow":
+        return "moo"
+    elif pet == "pig":
+        return "oink"
+    elif pet == "cat":
+        return "meow"
+    elif pet == "duck":
+        return "quack"
+    elif pet == "dog":
+        return "woof"
+
+def main():
+    animals = ["cow", "pig", "cat", "duck", "dog"]
+    for animal in animals:
+        print("Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!\n" \
+        "And on that farm he had a " + animal + ", Ee-igh, Ee-igh, Oh!\n" \
+        "With a " + oldMacDonald(animal) + ', ' + oldMacDonald(animal) + " here" \
+        " and a " + oldMacDonald(animal) + ', ' + oldMacDonald(animal) + " there.\n" \
+        "Here a " + oldMacDonald(animal) + ', there a ' + oldMacDonald(animal) \
+        + ", everywhere a " + oldMacDonald(animal) + ', ' + oldMacDonald(animal) \
+        + ".\n Old MacDonald had a farm, Ee-igh, Ee-igh, Oh!\n\n")
+    return
+
+if __name__ == "__main__":
+    main()
