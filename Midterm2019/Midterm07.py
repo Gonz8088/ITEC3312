fd4dca265a710d24081a8daa1072403036154253
Gonzales <212709006@logon.ds.ge.com>
2019-03-08 18:57:16 -0600

    finished midterm 07.

--- Midterm2019/Midterm07.py
+++ Midterm2019/Midterm07.py
@@ -3,9 +3,24 @@
 # ASSIGNMENT: Midterm #00
 # ALGORITHM: How the program works.  This should be structured using short, descriptive phrases that are indented appropriately.
 
-def count(s, ch):
-    
+def count(s: str, ch: str) -> int:
+    i = 0
+    count = 0
+    while True:
+        i = s.find(ch, i)
+        if i == -1:
+            break
+        else:
+            count += 1
+            i += 1
+    return count
+
 def main():
+    string = input("Enter a string: ")
+    substring = input("Enter a substring: ")
+
+    print("Ther are " + str(count(string, substring)) + " occurences of " + substring + " in " + string)
+    
     return
 
 if __name__ == "__main__":
