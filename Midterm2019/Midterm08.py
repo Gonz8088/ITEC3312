ba2fa174284d3216873afba9de9cf79509f7f0de
Gonzales <212709006@logon.ds.ge.com>
2019-03-09 09:29:33 -0600

    created stock class.

---
+++ Midterm2019/Midterm08.py
@@ -0,0 +1,18 @@
+# PROGRAMMER: Paul Gonzales
+# DATE: month day, 2019
+# ASSIGNMENT: Midterm 08
+# ALGORITHM: How the program works.  This should be structured using short, descriptive phrases that are indented appropriately.
+
+class Stock():
+    def __init__(self):
+        self.__symbol = ""
+        self.__name = ""
+        self.__previousClosingPrice = 0.0
+        self.__currentPrice = 0.0
+
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
