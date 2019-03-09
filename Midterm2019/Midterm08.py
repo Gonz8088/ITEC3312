be8dea27f3292320b6dab471b40b16d9454fb314
Gonzales <212709006@logon.ds.ge.com>
2019-03-09 09:38:54 -0600

    updated constructor signature.

--- Midterm2019/Midterm08.py
+++ Midterm2019/Midterm08.py
@@ -4,7 +4,7 @@
 # ALGORITHM: How the program works.  This should be structured using short, descriptive phrases that are indented appropriately.
 
 class Stock():
-    def __init__(self, name, symbol, prev_price, cur_price):
+    def __init__(self, name: str, symbol: str, prev_price: float, cur_price: float):
         self.__name = name
         self.__symbol = symbol
         self.__previousClosingPrice = prev_price
