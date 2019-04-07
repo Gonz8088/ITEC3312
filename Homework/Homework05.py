c001c96ab30e597770184f701808f379f529ec41
Gonzales <212709006@logon.ds.ge.com>
2019-04-07 17:02:34 -0500

    refactored into functions.

--- Homework/Homework05.py
+++ Homework/Homework05.py
@@ -3,26 +3,6 @@
 # ASSIGNMENT: Homework 05
 # ALGORITHM: How the program works.  This should be structured using short, descriptive phrases that are indented appropriately.
 
-
-
-#Download the TXBabyNames.txt file from Blackboard and put it into your TestData folder. Look at it, then write code to read it into a list.
-
-#Next, answer the following questions:
-
-#1.  How many girl babies have been born since 1910?
-
-#2.  How many boy babies have been born since 1910?
-
-#3.  How many girl babies were born 1910?  In 2012?
-
-#4.  How many boy babies were born in 1910?  In 2012?
-
-#5.  What are the total number of babies born in Texas in 2012?
-
-#6.  What are the total number of babies born in Texas with your name since 1910? #If you have an uncommon name, look for the first name of our new UHCL president - Ira.
-
-#7.  What are the total number of babies born in Texas with your name between 1910 and 1960? #If you have an uncommon name, look for the first name of our new UHCL president - Ira.
-
 #8.  What name was the most popular (had the highest count in one year): List the count for boys and then for girls.
 
 9.  What name was that? List the name that was the most popular for boys  and then for girls.
@@ -33,80 +13,78 @@
 
 # Write all this information out to a file.
 
+def gender_count(gender):
+    total = 0
+    for name in txnames:
+        record = name.strip().rpartition(',')
+        if record[0][3] == gender:
+            total += int(record[2])
+    return total
+
+def gender_count_by_year(gender, year):
+    total = 0
+    for name in txnames:
+        record = name.strip().rpartition(',')
+        if record[0][3] == gender and int(record[0][5:9]) == year:
+            total += int(record[2])
+    return total
+
+def count_by_year(year):
+    total = 0
+    for name in txnames:
+        record = name[i].strip().rpartition(',')
+        if int(record[0][5:9]) == year:
+            total += int(record[2])
+    return total
+
+def count_by_name(name):
+    total = 0
+    for name in txnames:
+        record = name[i].strip().rpartition(',')
+        if record[0][10:14] == "Paul":
+            total += int(record[2])
+    return total
+
+def name_count_by_range(name, a_year, b_year):
+    total = 0
+    for name in txnames:
+        record = name[i].strip().rpartition(',')
+        if record[0][10:14] == "Paul" and int(record[0][5:9]) >= a_year and int(record[0][5:9]) <= b_year:
+            total += int(record[2])
+    return total
+
+def most_popular_by_gender(gender):
+    most_pop = ("", ',', 0)
+    for name in txnames:
+        record = name.strip().rpartition(',')
+        if record[0][3] == gender and int(record[2]) > int(most_pop[2]):
+            most_pop = record
+    return most_pop
+
+def year_name_most_pop(name):
+    highest = ("", ',', 0)
+    for name in txnames:
+        record = name.strip().rpartition(',')
+        if record[0][10:14] == name and int(record[2]) > int(highest[2]):
+            highest = record
+    return highest
+
 def main():
     with open("TXBabyNames.txt", 'r') as names:
         txnames = names.readlines()
 
-    boys = 0
-    for i in range(len(txnames)):
-        record = txnames[i].strip().rpartition(',')
-        if record[0][3] == 'M':
-            boys += int(record[2])
-
-    girls = 0
-    for i in range(len(txnames)):
-        record = txnames[i].strip().rpartition(',')
-        if record[0][3] == 'F':
-            girls += int(record[2])
-
-    boys1910 = 0
-    for i in range(len(txnames)):
-        record = txnames[i].strip().rpartition(',')
-        if record[0][3] == 'M' and "1910" in record[0]:
-            boys1910 += int(record[2])
-    boys2012 = 0
-    for i in range(len(txnames)):
-        record = txnames[i].strip().rpartition(',')
-        if record[0][3] == 'M' and "2012" in record[0]:
-            boys2012 += int(record[2])
-
-    girls1910 = 0
-    for i in range(len(txnames)):
-        record = txnames[i].strip().rpartition(',')
-        if record[0][3] == 'F' and "1910" in record[0]:
-            girls1910 += int(record[2])
-
-    girls2012 = 0
-    for i in range(len(txnames)):
-        record = txnames[i].strip().rpartition(',')
-        if record[0][3] == 'F' and "2012" in record[0]:
-            girls2012 += int(record[2])
-
-    babys2012 = 0
-    for i in range(len(txnames)):
-        record = txnames[i].strip().rpartition(',')
-        if "2012" in record[0]:
-            babys2012 += int(record[2])
-
-    paul1910 = 0
-    for i in range(len(txnames)):
-        record = txnames[i].strip().rpartition(',')
-        if int(record[5:9]) >= 1910 and record[0][10:14] == "Paul":
-            paul1910 += int(record[2])
-
-    paul10thru60 = 0
-    for i in range(len(txnames)):
-        record = txnames[i].strip().rpartition(',')
-        if int(record[0][5:9]) >= 1910 and int(record[0][5:9]) <= 1960 and record[0][10:14] == "Paul":
-            paul10thru60 += int(record[2])
-
-    highestboy = ("", ',', 0)
-    for name in txnames:
-        record = name.strip().rpartition(',')
-        if record[0][3] == 'M' and int(record[2]) > int(highestboy[2]):
-            highestboy = record
-    
-    highestgirl = ("", ',', 0)
-    for name in txnames:
-        record = name.strip().rpartition(',')
-        if record[0][3] == 'F' and int(record[2]) > int(highestgirl[2]):
-            highestgirl = record
-                            
-    highestpaul = ("", ',', 0)
-    for name in txnames:
-        record = name.strip().rpartition(',')
-        if record[0][10:14] == "Paul" and int(record[2]) > int(highestpaul[2]):
-            highestpaul = record
+    total_boys = gender_count('M')
+    total_girls = gender_count('F')
+    tot_boys_1910 = gender_count_by_year('M', 1910)
+    tot_boys_2012 = gender_count_by_year('M', 2012)
+    tot_girls_1910 = gender_count_by_year('F', 2010)
+    tot_girls_2012 = gender_count_by_year('F', 2012)
+    tot_babys_2012 = count_by_year(2012)
+    tot_paul = count_by_name("Paul")
+    tot_paul_10_thru_60 = name_count_by_range("Paul", 1910, 1960)
+    popular_boy = most_popular_by_gender('M')
+    popular_girl = most_popular_by_gender('F')
+    year_paul_most_pop = year_name_most_pop("Paul")
 
     return
 
